d47dd7666521dcd9547c4cf3175d5fd2c5ba08b8
trust <trustadekoye@gmail.com>
2023-07-15 15:17:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ColorModeContext, useMode } from "./themes";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
+import Sidebar from "./scenes/global/Sidebar";
+import LoginPage from "./scenes/login";
+// import Team from "./scenes/Team";
+// import Contacts from "./scenes/Contacts";
+// import Invoices from "./scenes/Invoices";
+// import Form from "./scenes/Form";
+// import Bar from "./scenes/Bar";
+// import Pie from "./scenes/Pie";
+// import Line from "./scenes/Line";
+// import FAQ from "./scenes/FAQ";
 
 function App() {
+  const [ isAuthenticated, setIsAuthenticated ] = useState(false);
+  const [ theme, colorMode ] = useMode();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+        {isAuthenticated ? (
+              <Sidebar />
+            ) : (
+             null
+            )}
+          <main className="content">
+         {isAuthenticated ? (
+              <Topbar />
+            ) : (
+              null
+            )}
+            {isAuthenticated ? (
+              <Dashboard />
+            ) : (
+              <LoginPage setIsAuthenticated={setIsAuthenticated} />
+            )}
+            <Routes>
+              <Route path="/" element={< Dashboard />} />
+              {/* <Route path="/team" element={<Team />} /> */}
+              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+            </Routes>
+          </main>
+        </div>
+        </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
